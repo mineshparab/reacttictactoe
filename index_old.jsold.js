2425af0be6fa171780ef90d1684c0184961b9d23
Minesh <minash0078@gmail.com>
2024-03-02 21:14:09 -0600

    tictactoe commit

---
+++ index_old.jsold.js
@@ -0,0 +1,15 @@
+// npx create-react-app my-app  will creaet index.js related files
+
+
+// StrictMode is a tool for highlighting potential problems in an application. Like Fragment , StrictMode does not render any visible UI. 
+//It activates additional checks and warnings for its descendants. Note: Strict mode checks are run in development mode only; 
+//they do not impact the production build.
+
+import { StrictMode } from 'react';
+
+/*
+createRoot lets you create a root to display React components inside a browser DOM node.
+*/
+import { createRoot } from 'react-dom/client';
+import './style.css' ;
+import App from './App';
